588c998fd7c3d6ab6877c930c44c8c874e311596
Prachi Learns <prachi.bora@gmail.com>
2018-01-07 14:24:18 +0530

    adding selenium tests

---
+++ SeleniumTests/src/test/java/com/AzAppSeleniumTests/SeleniumTests/AppTest.java
@@ -0,0 +1,21 @@
+package com.AzAppSeleniumTests.SeleniumTests;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest
+{
+	@Test
+	public void testApp()
+    { 
+		System.setProperty("webdriver.chrome.driver", "D:\\Work\\VSTLM\\Demo Setup\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.compendiumdev.co.uk/selenium");
+		
+        assertTrue( driver.getTitle().startsWith("Selenium Simplified") );
+        driver.quit();
+    }
+}
